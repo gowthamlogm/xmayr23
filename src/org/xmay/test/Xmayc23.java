9fdaa9cdee1edf339bca0188417b0d28bf410f47
gowtham <gowthamlogm@gmail.com>
2023-05-29 13:38:23 +0530

    sample project

---
+++ src/org/xmay/test/Xmayc23.java
@@ -0,0 +1,15 @@
+package org.xmay.test;
+
+public class Xmayc23 {
+private void xmayName() {
+	System.out.println("gow");
+}	
+private void xmayId() {
+System.out.println("1234");
+}
+public static void main(String[] args) {
+	Xmayc23 x=new Xmayc23();
+	x.xmayName();
+	x.xmayId();
+}
+}
